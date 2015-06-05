d56d6f85ff854656c348f0f09876f7dab3f9a881
Kornelijus Survila <kornholijo@gmail.com>
2015-06-05 13:09:15 -0500

    Prevent missing the callback on the last node in the stream

--- src/opentibia/binaryfile.rs
+++ src/opentibia/binaryfile.rs
@@ -1,4 +1,7 @@
+use std::convert;
 use std::io;
+
+use byteorder::Error;
 use helpers::ReadExt;
 
 const NODE_ESCAPE: u8 = 0xFD;
@@ -13,75 +16,86 @@ pub struct Node {
 }
 
 impl Node {
-	pub fn deserialize(r: &mut io::Read, skip_start: bool) -> io::Result<Node> {
-		if !skip_start {
-			let data = try!(r.read_byte());
+    pub fn deserialize(r: &mut io::Read, skip_start: bool) -> io::Result<Node> {
+        if !skip_start {
+            let data = try!(r.read_byte());
 
-			if data != NODE_START {
-				let invalid_data_error: io::Error = io::Error::new(io::ErrorKind::Other, "unexpected data");
-				return Err(invalid_data_error)
-			}
-		}
+            if data != NODE_START {
+                let invalid_data_error: io::Error = io::Error::new(io::ErrorKind::Other, "unexpected data");
+                return Err(invalid_data_error)
+            }
+        }
 
-		let kind = try!(r.read_byte());
+        let kind = try!(r.read_byte());
 
-		let mut data = Vec::new();
-		let mut children = Vec::new();
+        let mut data = Vec::new();
+        let mut children = Vec::new();
 
-		loop {
-			let b = try!(r.read_byte());
+        loop {
+            let b = try!(r.read_byte());
 
-			match b {
-				NODE_START => children.push(try!(Node::deserialize(r, true))),
-				NODE_END => break,
-				NODE_ESCAPE	=> data.push(try!(r.read_byte())),
-				_ => data.push(b)
-			}
-		}
+            match b {
+                NODE_START => children.push(try!(Node::deserialize(r, true))),
+                NODE_END => break,
+                NODE_ESCAPE => data.push(try!(r.read_byte())),
+                _ => data.push(b)
+            }
+        }
 
-		data.shrink_to_fit();
-		children.shrink_to_fit();
+        data.shrink_to_fit();
+        children.shrink_to_fit();
 
-		Ok(Node {
-			kind: kind,
-			data: data,
-			children: children
-		})
-	}
+        Ok(Node {
+            kind: kind,
+            data: data,
+            children: children
+        })
+    }
 }
 
-pub fn streaming_parser<F>(r: &mut io::Read, skip_start: bool, callback: F) -> io::Result<()>
-	where F: Fn(u8, &[u8]) -> io::Result<bool> {
+pub fn streaming_parser<F>(r: &mut io::Read, skip_start: bool, mut callback: F) -> io::Result<()>
+    where F: FnMut(u8, &[u8]) -> io::Result<bool> {
 
-	if !skip_start {
-		let data = try!(r.read_byte());
+    if !skip_start {
+        let data = try!(r.read_byte());
 
-		if data != NODE_START {
-			let invalid_data_error: io::Error = io::Error::new(io::ErrorKind::Other, "unexpected data");
-			return Err(invalid_data_error)
-		}
-	}
+        if data != NODE_START {
+            let invalid_data_error: io::Error = io::Error::new(io::ErrorKind::Other, "unexpected data");
+            return Err(invalid_data_error)
+        }
+    }
 
-	let mut kind = try!(r.read_byte());
-	let mut data = Vec::new();
+    let mut kind = try!(r.read_byte());
+    let mut data = Vec::new();
 
-	loop {
-		let b = try!(r.read_byte());
+    loop {
+        let b = match r.read_byte() {
+            Ok(b) => b,
+            Err(Error::UnexpectedEOF) => {
+                // The last node is not yet processed at this point.
+                // We don't care about the result since this is at EOF.
+                callback(kind, &data[..]);
+                return Ok(())
+            },
+            Err(err) => return Err(convert::From::from(err))
+        };
 
-		match b {
-			NODE_START => {
-				if !try!(callback(kind, &data[..])) {
-					return Ok(())
-				}
-				
-				kind = try!(r.read_byte());
-				data.clear();
-			},
-			NODE_END => (),
-			NODE_ESCAPE	=> data.push(try!(r.read_byte())),
-			_ => data.push(b)
-		}
-	}
+        match b {
+            NODE_START => {
+                let callback_result = try!(callback(kind, &data[..]));
+                data.clear();
 
-	Ok(())
+                // Stop parsing if callback returned false
+                if !callback_result {
+                    return Ok(())
+                }
+
+                kind = try!(r.read_byte());
+            },
+
+            NODE_END => (),
+            NODE_ESCAPE => data.push(try!(r.read_byte())),
+            _ => data.push(b)
+        }
+    }
 }
