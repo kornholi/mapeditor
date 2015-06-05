aa799de48ee8b78a0bcb010eeb09ace12b14e14a
Kornelijus Survila <kornholijo@gmail.com>
2015-06-05 07:11:02 -0500

    Added Position struct

--- src/opentibia/mod.rs
+++ src/opentibia/mod.rs
@@ -1,1 +1,28 @@
+use std::io;
+use std::fmt;
+use helpers::ReadExt;
+
 pub mod binaryfile;
+
+#[derive(Debug)]
+pub struct Position {
+    pub x: u16,
+    pub y: u16,
+    pub z: u8
+}
+
+impl Position {
+	pub fn deserialize(r: &mut io::Read) -> io::Result<Position> {
+		Ok(Position {
+			x: try!(r.read_u16()),
+			y: try!(r.read_u16()),
+			z: try!(r.read_byte())
+		})
+	}
+}
+
+impl fmt::Display for Position	{
+	fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+		fmt.write_fmt(format_args!("{},{},{}", self.x, self.y, self.z))
+	}
+}
